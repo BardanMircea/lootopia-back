d00ebb038f475a257c5ec8f8f38d402e806d9da7
Mircea Bardan <mircea.c.bardan@gmail.com>
2025-07-13 21:12:02 +0200

    refactor for uniformity

--- src/main/java/com/sdv/lootopia/web/controller/ChasseController.java
+++ src/main/java/com/sdv/lootopia/web/controller/ChasseController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/chasses")
+@RequestMapping("/chasses")
 @RequiredArgsConstructor
 public class ChasseController {
 
